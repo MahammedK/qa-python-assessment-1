e6391ab6f69b318ec3e0e2d1ee90c301c0f219ea
Mahammed <mahammedkassam@gmail.com>
2022-03-25 14:01:32 +0000

    MK: completed Q2

--- questions/python1.py
+++ questions/python1.py
@@ -64,6 +64,16 @@ def one(input1, input2):
 
 	# No hints for this question
 
+def two(arg1):
+	if (arg1 % 5) != 0 and (arg1 % 3) == 0:
+		return "fizz"
+	elif (arg1 % 3) != 0 and (arg1 % 5) == 0:
+		return "buzz"
+	elif (arg1 % 5) == 0 and (arg1 % 3) == 0:
+		return "fizzbuzz"
+	else:
+		return "null"
+
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
 
 	# <QUESTION 3>
